1745dc8f2aff7272780719d48c605677c2662885
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-14 09:50:40 -0600

    update

--- app/services/airbender_service.rb
+++ app/services/airbender_service.rb
@@ -1,18 +1,22 @@
 class AirbenderService
   def nation_members(nation)
-    nation = nation.split("_")
+    #nation = nation.split("_")
     params = { affiliation: "#{nation[0]}+#{nation[1]}"}
-    get_json(/)
+    get_json('api/v1/characters', params)
   end
 
   private
 
   def get_json(url, params)
+    binding.pry
     response = conn.get(url, params)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(url: 'last-airbender-api.herokuapp.com/api/v1')
+    Faraday.new(url: 'https://last-airbender-api.herokuapp.com') do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
 
 end
